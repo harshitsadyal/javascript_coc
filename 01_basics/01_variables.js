f7d1af8e1b0206e439b01cc4ced4d5908f8d7e4c
harshitsadyal <harshitsadyal2@gmail.com>
2024-10-15 14:46:08 +0000

    variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 14453
+let accountEmail = "harshit@sadyal.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+accountEmail = "hc2hc.com"
+accountPassword = "212121"
+accountCity = "bengluru"
+
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
